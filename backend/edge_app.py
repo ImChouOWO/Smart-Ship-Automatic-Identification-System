1f22facc709f2f9a4b86e4db57684795560dbdf2
ImChouOWO <as>
2025-03-31 15:54:56 +0800

    Update imu function

--- backend/edge_app.py
+++ backend/edge_app.py
@@ -3,6 +3,8 @@ import socketio
 import time
 import random
 import time
+from imu import DueData
+import serial
 
 # 固定 IP server 的 IP，請替換成你的裝置 B IP
 SERVER_URL = 'http://140.133.74.176:5000'  # 或公開 IP
@@ -21,10 +23,27 @@ def disconnect():
 sio.connect(SERVER_URL)
 
 
+def get_imu_data():
+    port = '/dev/ttyUSB0' # USB serial port linux
+    baud = 9600   # Same baud rate as the INERTIAL navigation module
+    ser = serial.Serial(port, baud, timeout=0.5)
+    print("Serial is Opened:", ser.is_open)
+    while(1):
+        RXdata = ser.read(1)#一个一个读
+        RXdata = int(RXdata.hex(),16) #转成16进制显示
+        result = DueData(RXdata)
+        if result != None:
+            # print(result)
+            time.sleep(0.2)
+            print(type(result[2]))
+            return ['%.2f' % result[0], '%.2f' % result[1], '%.2f' % (result[2]-167)]
+        
+        return "No imu info"
+
 if __name__ =="__main__":
     while True:
         try:
-            data= "test imu data"
+            data= get_imu_data()
             sio.emit("get_imu",data)
             print(f"send:{data}")
             time.sleep(5)
