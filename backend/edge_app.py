0239fa67955d7a9c2372044bba3882e2da51388e
ImChouOWO <as>
2025-04-07 14:01:53 +0800

    Add GPS

--- backend/edge_app.py
+++ backend/edge_app.py
@@ -66,6 +66,54 @@ def imu_process_func():
     except Exception as e:
         print(f"❌ IMU process error: {e}")
 
+def parse_nmea_sentence(sentence):
+    if sentence.startswith('$GPGGA'):
+        parts = sentence.split(',')
+        if len(parts) > 6 and parts[6] == '1':
+            time_str = parts[1]
+            lat = parts[2]
+            lat_dir = parts[3]
+            lon = parts[4]
+            lon_dir = parts[5]
+            alt = parts[9]
+            return time_str, lat, lat_dir, lon, lon_dir, alt
+    return None, None, None, None, None, None
+
+def gps_process_func():
+    sio = create_sio()
+    prot = GPS
+    baud = 4800
+    try:
+        ser = serial.Serial(prot,baud, timeout=0.5)
+        print("✅ GPS Serial is Opened:", ser.is_open)
+        time.sleep(1)
+        while True:
+            try:
+                line = ser.readline().decode('ascii', errors='replace').strip()
+                if line:
+                    print(f"接收到的NMEA語句: {line}")
+                    time_str, lat, lat_dir, lon, lon_dir, alt = parse_nmea_sentence(line)
+                    if time_str and lat and lon:
+                        print(f"時間: {time_str}")
+                        print(f"緯度: {lat} {lat_dir}")
+                        print(f"經度: {lon} {lon_dir}")
+                        print(f"海拔: {alt} M")
+                        data = {
+                            "time":time_str,
+                            "latitude":lat,
+                            "longitude":lon,
+                            "altitude":alt
+                        }
+                        sio.emit("get_gps",data)
+                        time.sleep(5)
+            except ValueError:
+                print("無效的NMEA數據，繼續等待...")
+                continue             
+    except Exception as e:
+        print(f'erro:{e}')
+
+
+
 def push_video_process_func():
     sio = create_sio()
     sio.emit("get_video_info", {"device": "edge_01", "url": RTSP_URL})
