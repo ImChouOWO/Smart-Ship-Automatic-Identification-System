c32504c5c8a4a7470baebb48ed20cec17e7f4d2d
ImChouOWO <as>
2025-04-08 15:07:37 +0800

    Fix IMU process crash

--- backend/app.py
+++ backend/app.py
@@ -114,4 +114,4 @@ def start_rtsp_server():
 
 if __name__ == '__main__':
     start_rtsp_server()
-    socketio.run(app, debug=True, host='0.0.0.0', port=5000)
+    socketio.run(app, debug=False, host='0.0.0.0', port=5000)
