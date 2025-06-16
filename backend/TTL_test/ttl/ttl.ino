b9c66b0769c9bcf5c0ac9e3ba359d4626e00720e
ImChouOWO <as>
2025-06-16 14:23:22 +0800

    Update TTL send

--- backend/TTL_test/ttl/ttl.ino
+++ backend/TTL_test/ttl/ttl.ino
@@ -1,8 +1,19 @@
 void setup() {
-  Serial.begin(9600);  // 開啟 Serial 通訊
+  Serial.begin(9600);
 }
 
 void loop() {
-  Serial.println("Hello from Arduino");
-  delay(1000); // 每秒傳送一次
+  while (Serial.available() > 0) {
+    byte incomingByte = Serial.read();
+
+    Serial.print("0x");
+    if (incomingByte < 0x10) Serial.print("0");
+    Serial.print(incomingByte, HEX);
+    Serial.print(" ");
+
+    if (incomingByte == 0x6E) {
+      Serial.println();  // 當遇到封包結尾字元時換行
+    }
+  }
 }
+                                          
